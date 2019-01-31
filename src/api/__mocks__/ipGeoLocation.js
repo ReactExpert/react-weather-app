8a74665e519b2fc0ce1da08d6e6406b41082f54d
Henry Esparza <ReactExpert@users.noreply.github.com>
2019-01-31 15:30:06 +0800

    delete

--- src/api/__mocks__/ipGeoLocation.js
+++
@@ -1,6 +0,0 @@
-import data from '../../mock/ipGeoLocation';
-
-const fetch = jest.fn(() => Promise.resolve({...data}));
-const mock = jest.fn().mockImplementation(() => ({ fetch: fetch, data: data }));
-
-export default mock;
